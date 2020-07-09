7e419122ccb5d72bece85f7371eaddb1244b4b52
Yang You <tg824074@login1.longhorn.tacc.utexas.edu>
2020-07-09 00:39:32 -0500

    u

--- cp_imagenet_to_temp.sh
+++ cp_imagenet_to_temp.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Copying imagenet.tar to /tmp/"
-cp /scratch/00946/zzhang/data/imagenet-1k.tar /tmp/
+echo "Copying imagenet.tar to /tmp/test/"
+rm -rf /tmp/test/
+mkdir /tmp/test/
+cp /scratch/00946/zzhang/data/imagenet-1k.tar /tmp/test/
 echo "Done copying. Extracting"
-tar xf /tmp/imagenet-1k.tar -C /tmp/
+tar xf /tmp/test/imagenet-1k.tar -C /tmp/test/
 echo "Done."
